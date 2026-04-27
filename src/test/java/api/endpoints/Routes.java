00320bb9cd40dd1abd601c73b4e7e855043f973c
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2026-04-27 22:01:10 +0600

    Add store module

--- src/test/java/api/endpoints/Routes.java
+++ src/test/java/api/endpoints/Routes.java
@@ -20,4 +20,12 @@ public class Routes {
 	public static  String update_url = base_url +"/user/{username}";
 	public static  String delete_url = base_url +"/user/{username}";
 	
+	// store module
+	public static String getInventory_URL = base_url + "/store/inventory";
+	public static String postOrder_URL = base_url + "/store/order";
+	public static String getOrder_URL = base_url + "/store/order/{orderId}";
+	public static String DeleteOrder_URL = base_url + "/store/{orderId}";
+
+	
+	
 }
